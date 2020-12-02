4b885af8227fea6c5cbb319b4b18a815ff27ffb3
Ferdinand Linnenberg <ferdinand.linnenberg@aol.com>
2020-12-02 13:19:47 +0100

    even more cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -1,28 +1,20 @@
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<i32>{
-    input.split('\n').filter(
-        |p|{
-            !p.is_empty()
-        }
-    ).map(|c| c.parse().expect("Couldn't parse number")).collect()
+pub fn input_generator(input: &str) -> Vec<i32> {
+    input
+        .split('\n')
+        .filter(|p| !p.is_empty())
+        .map(|c| c.parse().expect("Couldn't parse number"))
+        .collect()
 }
 
 #[aoc(day1, part1)]
-pub fn solve_part1(numbers: &[i32]) -> i32{
+pub fn solve_part1(numbers: &[i32]) -> i32 {
     for i in 0..numbers.len() {
         let n_current = numbers[i];
         for n_current_2 in numbers.iter().skip(i) {
             let nx = n_current + n_current_2;
             if nx == 2020 {
-                /*
-                println!(
-                    "{} + {} -> 2020 | {}",
-                    n_current,
-                    n_current_2,
-                    n_current * n_current_2
-                );
-                 */
-                return n_current*n_current_2
+                return n_current * n_current_2;
             }
         }
     }
@@ -30,7 +22,7 @@ pub fn solve_part1(numbers: &[i32]) -> i32{
 }
 
 #[aoc(day1, part2)]
-pub fn solve_part2(numbers: &[i32]) -> i32{
+pub fn solve_part2(numbers: &[i32]) -> i32 {
     for i in 0..numbers.len() {
         let n_current = numbers[i];
         for i2 in i..numbers.len() {
@@ -40,16 +32,7 @@ pub fn solve_part2(numbers: &[i32]) -> i32{
                 for n_current_3 in numbers.iter().skip(i2) {
                     let ixx = nx + n_current_3;
                     if ixx == 2020 {
-                        /*
-                        println!(
-                            "{} + {} + {} -> 2020 | {}",
-                            n_current,
-                            n_current_2,
-                            n_current_3,
-                            n_current * n_current_2 * n_current_3
-                        );
-                         */
-                        return  n_current * n_current_2 * n_current_3;
+                        return n_current * n_current_2 * n_current_3;
                     }
                 }
             }
--- src/day2.rs
+++ src/day2.rs
@@ -9,78 +9,89 @@ pub struct Password {
     pub policy_max: u32,
 }
 #[aoc_generator(day2)]
-pub fn input_generator(input: &str) -> Vec<Password>{
-    input.split('\n').filter_map(|f|{
-        return if f.is_empty() {
-            None
-        } else {
-            let splits = f.split(':').collect::<Vec<&str>>();
+pub fn input_generator(input: &str) -> Vec<Password> {
+    input
+        .split('\n')
+        .filter_map(|f| {
+            return if f.is_empty() {
+                None
+            } else {
+                let splits = f.split(':').collect::<Vec<&str>>();
 
-            let password = splits.get(1).unwrap().trim();
-            let policy_splits = splits.get(0).unwrap().split(' ').collect::<Vec<&str>>();
+                let password = splits.get(1).unwrap().trim();
+                let policy_splits = splits.get(0).unwrap().split(' ').collect::<Vec<&str>>();
 
-            let policy_char = policy_splits.get(1).unwrap();
-            let policy_amount_splits = policy_splits
-                .get(0)
-                .unwrap()
-                .split('-')
-                .collect::<Vec<&str>>();
-            let min = policy_amount_splits.get(0).unwrap();
-            let max = policy_amount_splits.get(1).unwrap();
+                let policy_char = policy_splits.get(1).unwrap();
+                let policy_amount_splits = policy_splits
+                    .get(0)
+                    .unwrap()
+                    .split('-')
+                    .collect::<Vec<&str>>();
+                let min = policy_amount_splits.get(0).unwrap();
+                let max = policy_amount_splits.get(1).unwrap();
 
-            Some(Password {
-                password: password.to_string(),
-                policy_char: policy_char.chars().next().unwrap(),
-                policy_min: min.parse().unwrap(),
-                policy_max: max.parse().unwrap(),
-            })
-        }
-    }).collect()
+                Some(Password {
+                    password: password.to_string(),
+                    policy_char: policy_char.chars().next().unwrap(),
+                    policy_min: min.parse().unwrap(),
+                    policy_max: max.parse().unwrap(),
+                })
+            };
+        })
+        .collect()
 }
 
 #[aoc(day2, part1)]
 pub fn solve_part1(passwords: &[Password]) -> usize {
-    passwords.iter().filter(|p|{
-        let mut chars_found = 0;
-        for c in p.password.chars() {
-            if c == p.policy_char {
-                chars_found += 1;
+    passwords
+        .iter()
+        .filter(|p| {
+            let mut chars_found = 0;
+            for c in p.password.chars() {
+                if c == p.policy_char {
+                    chars_found += 1;
+                }
             }
-        }
-        if chars_found >= p.policy_min && chars_found <= p.policy_max {
-            return true
-        }
+            if chars_found >= p.policy_min && chars_found <= p.policy_max {
+                return true;
+            }
 
-        false
-    }).collect::<Vec<&Password>>().len()
+            false
+        })
+        .collect::<Vec<&Password>>()
+        .len()
 }
 
 #[aoc(day2, part2)]
 pub fn solve_part2(passwords: &[Password]) -> usize {
-    passwords.iter().filter(|p|{
-        let char_vec = p.password.chars().collect::<Vec<char>>();
-        let mut found = 0;
-        match char_vec.get((p.policy_min - 1) as usize) {
-            Some(v) => {
-                if v == &p.policy_char {
-                    found += 1;
+    passwords
+        .iter()
+        .filter(|p| {
+            let char_vec = p.password.chars().collect::<Vec<char>>();
+            let mut found = 0;
+            match char_vec.get((p.policy_min - 1) as usize) {
+                Some(v) => {
+                    if v == &p.policy_char {
+                        found += 1;
+                    }
+                }
+                _ => {
+                    return false;
                 }
             }
-            _ => {
-                return false;
-            }
-        }
-        match char_vec.get((p.policy_max - 1) as usize) {
-            Some(v) => {
-                if v == &p.policy_char {
-                    found += 1;
+            match char_vec.get((p.policy_max - 1) as usize) {
+                Some(v) => {
+                    if v == &p.policy_char {
+                        found += 1;
+                    }
+                }
+                _ => {
+                    return false;
                 }
             }
-            _ => {
-                return false;
-            }
-        }
 
-        found == 1
-    }).collect::<Vec<&Password>>().len()
+            found == 1
+        })
+        .collect::<Vec<&Password>>()
+        .len()
 }
