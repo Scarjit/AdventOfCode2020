460fe37f53559773c6751b81e7766185d51d1a10
Ferdinand Linnenberg <ferdinand.linnenberg@aol.com>
2020-12-02 10:20:07 +0100

    Day 2 & cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 use crate::day1::Day1;
-mod day1;
+use crate::day2::Day2;
 
-trait RunnableDay{
-    fn run(&self) -> Result<(),std::io::Error>{
+mod day1;
+mod day2;
+
+trait RunnableDay {
+    fn run(&self) -> Result<(), std::io::Error> {
         Ok(())
     }
 }
 
-pub(crate) fn read_input(day: i32) -> Result<Vec<String>,std::io::Error> {
-    Ok(std::fs::read_to_string("/home/scarjit/CLionProjects/adventofcode/src/day1/input")?.split('\n').map(
-        |c| String::from(c)
-    ).collect())
+pub(crate) fn read_input(day: i32) -> Result<Vec<String>, std::io::Error> {
+    Ok(std::fs::read_to_string(format!(
+        "/home/scarjit/CLionProjects/adventofcode/src/day{}/input",
+        day
+    ))?
+    .split('\n')
+    .map(|c| String::from(c))
+    .collect())
 }
 
 fn main() {
     let day1 = Day1::default();
     day1.run().expect("Day1 failed to run");
+
+    let day2 = Day2::default();
+    day2.run().expect("Day 2 failed to run");
 }
