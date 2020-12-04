f13f7d2a267c2187e991259814c48b0f5e029d46
Ferdinand Linnenberg <ferdinand.linnenberg@aol.com>
2020-12-04 17:50:27 +0100

    cargo fmt

--- src/day4.rs
+++ src/day4.rs
@@ -1,41 +1,31 @@
 #[derive(Debug, Clone)]
-pub enum Height{
+pub enum Height {
     Centimeter(u32),
     Inch(u32),
-    Unknown
+    Unknown,
 }
 
-impl Height{
-    pub fn parse_str(inp: &str) -> Height{
+impl Height {
+    pub fn parse_str(inp: &str) -> Height {
         if inp.len() < 3 {
             return Self::Unknown;
         }
-        let split = inp.split_at(inp.len()-2);
+        let split = inp.split_at(inp.len() - 2);
         let h = match split.0.parse::<u32>() {
-            Ok(v) => {
-                v
-            }
-            Err(_) => {
-                return Self::Unknown
-            }
+            Ok(v) => v,
+            Err(_) => return Self::Unknown,
         };
 
         match split.1 {
-            "cm" => {
-                Self::Centimeter(h)
-            }
-            "in" => {
-                Self::Inch(h)
-            }
-            _ => {
-                Self::Unknown
-            }
+            "cm" => Self::Centimeter(h),
+            "in" => Self::Inch(h),
+            _ => Self::Unknown,
         }
     }
 }
 
 #[derive(Debug, Clone)]
-pub enum EyeColor{
+pub enum EyeColor {
     Amber,
     Blue,
     Brown,
@@ -43,37 +33,26 @@ pub enum EyeColor{
     Green,
     Hazel,
     Other,
-    Unknown
+    Unknown,
 }
 
-impl EyeColor{
-    pub fn parse_str(inp: &str) -> Self{
+impl EyeColor {
+    pub fn parse_str(inp: &str) -> Self {
         match inp {
-            "amb" => {
-                Self::Amber
-            },"blu" => {
-                Self::Blue
-            },"brn" => {
-                Self::Brown
-            },"gry" => {
-                Self::Grey
-            },"grn" => {
-                Self::Green
-            },"hzl" => {
-                Self::Hazel
-            },
-            "oth" => {
-                Self::Other
-            }
-            _ => {
-                Self::Unknown
-            }
+            "amb" => Self::Amber,
+            "blu" => Self::Blue,
+            "brn" => Self::Brown,
+            "gry" => Self::Grey,
+            "grn" => Self::Green,
+            "hzl" => Self::Hazel,
+            "oth" => Self::Other,
+            _ => Self::Unknown,
         }
     }
 }
 
 #[derive(Debug, Clone, Default)]
-pub struct Passport{
+pub struct Passport {
     byr: Option<u32>,
     iyr: Option<u32>,
     eyr: Option<u32>,
@@ -84,12 +63,13 @@ pub struct Passport{
     //cid: Option<String>
 }
 
-impl Passport{
-    pub fn parse_splits(inp: &str) -> Option<Self>{
+impl Passport {
+    pub fn parse_splits(inp: &str) -> Option<Self> {
         let mut passport: Passport = Passport::default();
-        let invec = inp.split(|c| c == '\n' || c == ' ').map(|s| {
-            s.split(':').take(2).collect::<Vec<&str>>()
-        }).collect::<Vec<Vec<&str>>>();
+        let invec = inp
+            .split(|c| c == '\n' || c == ' ')
+            .map(|s| s.split(':').collect::<Vec<&str>>())
+            .collect::<Vec<Vec<&str>>>();
 
         for sx in invec {
             match sx[0] {
@@ -115,7 +95,7 @@ impl Passport{
                     passport.pid = Some(sx[1].to_string());
                 }
                 "cid" => {
-                    continue
+                    continue;
                     //passport.cid = Some(sx[1].to_string())
                 }
                 _ => {
@@ -128,106 +108,72 @@ impl Passport{
     }
 
     #[inline]
-    pub fn has_req_fields(&self) -> bool{
-           self.byr.is_some()
-        && self.iyr.is_some()
-        && self.eyr.is_some()
-        && self.hgt.is_some()
-        && self.hcl.is_some()
-        && self.ecl.is_some()
-        && self.pid.is_some()
+    fn has_req_fields(&self) -> bool {
+        self.byr.is_some()
+            && self.iyr.is_some()
+            && self.eyr.is_some()
+            && self.hgt.is_some()
+            && self.hcl.is_some()
+            && self.ecl.is_some()
+            && self.pid.is_some()
     }
 
-    fn validate_byr(&self) -> bool{
+    fn validate_byr(&self) -> bool {
         match self.byr {
-            None => {
-                false
-            }
-            Some(v) => {
-                v >= 1920 && v <= 2002
-            }
+            None => false,
+            Some(v) => v >= 1920 && v <= 2002,
         }
     }
 
-    fn validate_iyr(&self) -> bool{
+    fn validate_iyr(&self) -> bool {
         match self.iyr {
-            None => {
-                false
-            }
-            Some(v) => {
-                v >= 2010 && v <= 2020
-            }
+            None => false,
+            Some(v) => v >= 2010 && v <= 2020,
         }
     }
 
-    fn validate_eyr(&self) -> bool{
+    fn validate_eyr(&self) -> bool {
         match self.eyr {
-            None => {
-                false
-            }
-            Some(v) => {
-                v >= 2020 && v <= 2030
-            }
+            None => false,
+            Some(v) => v >= 2020 && v <= 2030,
         }
     }
 
-    fn validate_hgt(&self) -> bool{
+    fn validate_hgt(&self) -> bool {
         match &self.hgt {
-            None => {
-                false
-            }
-            Some(v) => {
-                match v{
-                    Height::Centimeter(c) => {
-                        c >= &150 && c <= &193
-                    }
-                    Height::Inch(i) => {
-                        i >= &59 && i <= &76
-                    }
-                    Height::Unknown => {
-                        false
-                    }
-                }
-            }
+            None => false,
+            Some(v) => match v {
+                Height::Centimeter(c) => c >= &150 && c <= &193,
+                Height::Inch(i) => i >= &59 && i <= &76,
+                Height::Unknown => false,
+            },
         }
     }
 
-    fn validate_hcl(&self) -> bool{
-        match &self.hcl{
-            None => {
-                false
-            }
+    fn validate_hcl(&self) -> bool {
+        match &self.hcl {
+            None => false,
             Some(hexstr) => {
-                hexstr.starts_with('#') && hexstr.len() == 7 && hexstr[6..].chars().all(|c|{
-                    c.is_ascii_hexdigit()
-                })
+                hexstr.starts_with('#')
+                    && hexstr.len() == 7
+                    && hexstr[6..].chars().all(|c| c.is_ascii_hexdigit())
             }
         }
     }
 
-    fn validate_ecl(&self) -> bool{
+    fn validate_ecl(&self) -> bool {
         match &self.ecl {
-            None => {
-                false
-            }
-            Some(v) => {
-                match v {
-                    EyeColor::Unknown => {
-                        false
-                    }
-                    _ => {
-                        true
-                    }
-                }
-            }
+            None => false,
+            Some(v) => match v {
+                EyeColor::Unknown => false,
+                _ => true,
+            },
         }
     }
 
     fn validate_pid(&self) -> bool {
         match &self.pid {
-            None => {
-                false
-            }
+            None => false,
             Some(v) => {
                 if v.len() != 9 {
                     return false;
@@ -237,56 +183,44 @@ impl Passport{
         }
     }
 
-
-    fn is_valid(&self) -> bool{
+    fn is_valid(&self) -> bool {
         self.validate_byr()
-        && self.validate_iyr()
-        && self.validate_eyr()
-        && self.validate_hgt()
-        && self.validate_hcl()
-        && self.validate_ecl()
-        && self.validate_pid()
+            && self.validate_iyr()
+            && self.validate_eyr()
+            && self.validate_hgt()
+            && self.validate_hcl()
+            && self.validate_ecl()
+            && self.validate_pid()
     }
 }
 
 #[aoc_generator(day4)]
-pub fn input_generator(input: &str) -> Vec<Option<Passport>>{
-    let parts = input.split("\n\n").map(|l|{
-        Passport::parse_splits(l)
-    }).collect::<Vec<Option<Passport>>>();
+pub fn input_generator(input: &str) -> Vec<Option<Passport>> {
+    let parts = input
+        .split("\n\n")
+        .map(|l| Passport::parse_splits(l))
+        .collect::<Vec<Option<Passport>>>();
     parts
 }
-
 
 #[aoc(day4, part1)]
 pub fn solve_part_1(input: &Vec<Option<Passport>>) -> usize {
     input
         .iter()
-        .filter(|f|{
-            match f {
-                None => {
-                    false
-                }
-                Some(v) => {
-                    v.has_req_fields()
-                }
-            }
-        }).count()
+        .filter(|f| match f {
+            None => false,
+            Some(v) => v.has_req_fields(),
+        })
+        .count()
 }
-
 
 #[aoc(day4, part2)]
 pub fn solve_part_2(input: &Vec<Option<Passport>>) -> usize {
     input
         .iter()
-        .filter(|f|{
-            match f {
-                None => {
-                    false
-                }
-                Some(v) => {
-                    v.is_valid()
-                }
-            }
-        }).count()
+        .filter(|f| match f {
+            None => false,
+            Some(v) => v.is_valid(),
+        })
+        .count()
 }
