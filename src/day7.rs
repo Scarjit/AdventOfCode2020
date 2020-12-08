2825ed9af4232d4dea92a5d3156a6b8d9d1247c6
Ferdinand Linnenberg <ferdinand.linnenberg@aol.com>
2020-12-08 14:54:40 +0100

    Day8

---
+++ src/day7.rs
@@ -0,0 +1,77 @@
+use pathfinding::directed::topological_sort::topological_sort;
+use regex::Regex;
+use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
+
+type Bags = HashMap<String, Vec<InnerBag>>;
+
+#[derive(Debug, Clone)]
+pub struct InnerBag {
+    name: String,
+    count: usize,
+}
+
+#[aoc_generator(day7)]
+pub fn input_generator(input: &str) -> HashMap<String, Vec<InnerBag>> {
+    let regex_outer_bag = Regex::new(r"^(.*) bags contain (.*)$").unwrap();
+    let regex_inner_bags = Regex::new(r"(\d+) (.*?) bag").unwrap();
+
+    input
+        .lines()
+        .map(|l| {
+            (
+                regex_outer_bag.captures(l).unwrap()[1].to_string(),
+                regex_inner_bags
+                    .captures_iter(l)
+                    .map(|c| InnerBag {
+                        name: c[2].to_string(),
+                        count: c[1].parse().unwrap(),
+                    })
+                    .collect(),
+            )
+        })
+        .collect()
+}
+
+fn sort_rev(bags: &Bags) -> Vec<&str> {
+    let mut sorted = topological_sort(
+        &bags.keys().map(|b| b.as_str()).collect::<Vec<_>>(), |&c| {
+        bags[c].iter().map(|d| d.name.as_str())
+    })
+    .unwrap();
+    sorted.reverse();
+    sorted
+}
+
+#[aoc(day7, part1)]
+pub fn solve_part_1(input: &Bags) -> usize {
+    let mut shinygoldhs = HashSet::new();
+    shinygoldhs.insert("shiny gold");
+
+    for bi in sort_rev(input).iter() {
+        if input[*bi]
+            .iter()
+            .any(|d| shinygoldhs.contains(d.name.as_str()))
+        {
+            shinygoldhs.insert(bi);
+        }
+    }
+
+    shinygoldhs.len() - 1
+}
+
+#[aoc(day7, part2)]
+pub fn solve_part_2(input: &Bags) -> usize {
+
+    let mut contains: HashMap<&str, HashMap<&str, usize>> = HashMap::new();
+    for c in sort_rev(input).into_iter() {
+        let mut inside = HashMap::new();
+        for ib in input[c].iter() {
+            *inside.entry(ib.name.as_str()).or_insert(0) += ib.count;
+            for (&dd, nn) in contains[&ib.name.as_str()].iter() {
+                *inside.entry(dd).or_insert(0) += ib.count * nn;
+            }
+        }
+        contains.insert(c, inside.into_iter().collect());
+    }
+    contains["shiny gold"].iter().map(|(_, &n)| n).sum()
+}
---
+++ src/day8.rs
@@ -0,0 +1,128 @@
+use std::str::FromStr;
+use stackvec::TryCollect;
+use std::collections::{HashMap};
+
+#[derive(Debug, Clone, PartialOrd, PartialEq)]
+pub enum Instructions{
+    acc(isize),
+    jmp(isize),
+    nop(isize)
+}
+
+impl FromStr for Instructions{
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let splits = s.split(' ').try_collect::<[&str;2]>().unwrap();
+        Ok(match splits[0] {
+            "acc" => {
+                Self::acc(splits[1].parse().unwrap())
+            }
+            "jmp" => {
+                Self::jmp(splits[1].parse().unwrap())
+            }
+            "nop" => {
+                Self::nop(splits[1].parse().unwrap())
+            }
+            _ => {
+                panic!()
+            }
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Program{
+    instructions: Vec<Instructions>,
+    current_pointer: usize,
+    accumulator: isize,
+    trace: HashMap<usize, Instructions>
+}
+
+impl Program{
+    pub fn step_once(&mut self){
+        let current_instruction = &self.instructions[self.current_pointer];
+        match current_instruction {
+            Instructions::acc(i) => {
+                self.accumulator += i;
+                self.current_pointer += 1;
+            }
+            Instructions::jmp(i) => {
+                let next_pointer: isize = self.current_pointer as isize + i;
+                self.current_pointer = next_pointer as usize;
+            }
+            Instructions::nop(_) => {
+                self.current_pointer += 1;
+            }
+        }
+    }
+
+    //Returns true if program terminates
+    pub fn run_program(&mut self) -> bool{
+        loop {
+            if self.current_pointer >= self.instructions.len() {
+                return true;
+            }
+            if self.trace.contains_key(&self.current_pointer) {
+                return false
+            }
+            self.trace.insert(self.current_pointer, self.instructions[self.current_pointer].clone());
+            self.step_once();
+        }
+    }
+}
+
+#[aoc_generator(day8)]
+pub fn input_generator(input: &str) -> Program {
+    let instructions = input
+        .lines()
+        .map(|l| {
+            Instructions::from_str(l).unwrap()
+        }).collect::<Vec<Instructions>>();
+    let len = &instructions.len();
+    Program{
+        instructions,
+        current_pointer: 0,
+        accumulator: 0,
+        trace: HashMap::with_capacity(*len)
+    }
+}
+
+#[aoc(day8, part1)]
+pub fn solve_part_1(input: &Program) -> isize {
+    let mut prg = input.clone();
+    prg.run_program();
+    prg.accumulator
+}
+
+#[aoc(day8, part2)]
+pub fn solve_part_2(input: &Program) -> isize {
+    let mut prg = input.clone();
+    prg.run_program();
+
+    for t in &prg.trace {
+        let new_instruction = match t.1 {
+            Instructions::jmp(i) => {
+                Instructions::nop(*i)
+            }
+            Instructions::nop(i) => {
+                Instructions::jmp(*i)
+            }
+            _ => {
+                continue
+            }
+        };
+
+        let mut p_clone = prg.clone();
+        p_clone.trace.clear();
+        p_clone.current_pointer = 0;
+        p_clone.accumulator = 0;
+        p_clone.instructions[*t.0] = new_instruction;
+
+        if p_clone.run_program() {
+            return p_clone.accumulator;
+        }
+    }
+
+    0
+}
