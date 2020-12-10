2713414eb71636123be157ab9c31f95728d942f7
Ferdinand Linnenberg <ferdinand.linnenberg@aol.com>
2020-12-10 17:49:34 +0100

    day10/part2

--- src/day10.rs
+++ src/day10.rs
@@ -1,19 +1,21 @@
 #[aoc_generator(day10)]
 pub fn input_generator(input: &str) -> Vec<usize> {
-    let mut adapters = input
+    let adapters = input
         .lines()
         .map(|l|{
             l.parse().unwrap()
         }).collect::<Vec<usize>>();
-    adapters.push(0);
-    adapters.sort_unstable();
-    adapters.push(adapters.last().unwrap()+3);
     adapters
 }
 
 #[aoc(day10, part1)]
 pub fn solve_part_1(input: &[usize]) -> usize {
-    let diffs = input.windows(2).map(|w| w[1]-w[0]).collect::<Vec<usize>>();
+    let mut adapters:Vec<usize> = input.to_vec();
+    adapters.push(0);
+    adapters.sort_unstable();
+    adapters.push(adapters.last().unwrap()+3);
+
+    let diffs = adapters.windows(2).map(|w| w[1]-w[0]).collect::<Vec<usize>>();
     let c1 = diffs.iter().filter(|x| x == &&1).count();
     let c3 = diffs.iter().filter(|x| x == &&3).count();
 
@@ -22,5 +24,20 @@ pub fn solve_part_1(input: &[usize]) -> usize {
 
 #[aoc(day10, part2)]
 pub fn solve_part_2(input: &[usize]) -> usize {
-    0
+    let mut adapters:Vec<usize> = input.to_vec();
+    adapters.sort_unstable();
+
+    let mut v = Vec::with_capacity(adapters.len()+1);
+    v.push((0, 1usize));
+
+    for adapter in adapters {
+        let sum = v.iter().rev().take(3)
+            .take_while(|(c,_)| {
+                c+3 >= adapter
+            })
+            .map(|(_,s)|s).sum();
+        v.push((adapter, sum));
+    }
+
+    v.last().unwrap().1
 }
